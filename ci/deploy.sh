b8086628fe39a43e8146008b8aae67824f89378d
MAC <lyyubava@gmail.com>
2024-04-17 02:01:20 +0300

    devops-0: final infra

--- ci/deploy.sh
+++ ci/deploy.sh
@@ -5,7 +5,7 @@ set -u
 
 scriptpath="$(readlink -f "$0")"
 workdir="${scriptpath%/*}"
-cd "${workdir}/../../ansible"
+cd "${workdir}/../ansible"
 
 echo "${ANSIBLE_VAULT_PASSWORD}" > .password
 ansible-playbook -e environment_name="${ENVIRONMENT_NAME}" -e frontend_port="${FRONTEND_PORT}" -e backend_port="${BACKEND_PORT}" -e app_sever=localhost -e app_domain="${APP_DOMAIN}" playbooks/nginx.yml
